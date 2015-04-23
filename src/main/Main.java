13e1bb4efc7c8d6eabb33a5109593e090b750e2f
Dennisluiten <dennisluiten1992@gmail.com>
2015-04-23 21:57:24 +0200

    Eerste stuffies

---
+++ src/main/Main.java
@@ -0,0 +1,60 @@
+package main;
+
+import java.io.IOException;
+
+import weka.classifiers.Classifier;
+import weka.classifiers.bayes.BayesNet;
+import weka.core.Attribute;
+import weka.core.FastVector;
+import weka.core.Instances;
+import weka.core.SerializationHelper;
+
+public class Main {
+	private MyFileReader fr;
+	public  FastVector wekaAttributes = new FastVector(94);
+	private Instances trainSet;
+	private boolean train = true, test = false;
+	private Classifier classifier;
+	
+	public static void main(String[] args) throws Exception {
+		new Main();
+		System.out.println("Processes finished.");
+	}
+	
+	public Main () throws Exception{
+		declareFeatureVector();
+		if(train){
+			fr = new MyFileReader("train", this);	
+			trainSet = new Instances ("Rel", wekaAttributes, 144368);
+			for(int i = 0; i < 61878; i++){
+				//System.out.println("INstance nr: " + i);
+				trainSet.add(fr.readInstanceTrain());
+			}
+			trainSet.setClassIndex(0);
+			classifier = new BayesNet();
+			classifier.buildClassifier(trainSet);
+			SerializationHelper.write("resources\\classifier.model", classifier);
+		}else{
+			classifier = (Classifier)SerializationHelper.read("resources\\classifier.model");
+		}
+		if(test){
+			//TODO
+		}
+	}
+	
+	private void declareFeatureVector(){
+		
+		FastVector classVector = new FastVector(9);
+		for(int i = 1; i <= 9; i++){
+			classVector.addElement("Class_" + i);
+		}
+		Attribute classAttribute = new Attribute ( "Class", classVector);
+		wekaAttributes.addElement(classAttribute);
+		for(int i = 1; i <= 93; i++){
+			wekaAttributes.addElement(new Attribute("Feature" + i));
+			
+		}
+		
+	}
+
+}
---
+++ src/main/MyFileReader.java
@@ -0,0 +1,50 @@
+package main;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import weka.core.Attribute;
+import weka.core.Instance;
+
+public class MyFileReader {
+	private File file;
+	private BufferedReader reader;
+	private Main main;
+	public final static String CONTENT = "content", CLASSIFICATION = "classifications";
+	
+	public MyFileReader (String testOrTrain, Main main) throws IOException{
+		this.main = main;
+		file = new File("resources\\"+ testOrTrain + ".csv");
+		try {
+			reader = new BufferedReader(new FileReader(file));
+			System.out.println(reader.readLine());
+		} catch (FileNotFoundException e) {
+			System.out.println("Er is geen file te vinden.");
+			e.printStackTrace();
+		}
+	}
+	
+	public Instance readInstanceTrain() throws IOException{
+		Instance instance = new Instance(94);
+		String nextLine = reader.readLine();
+//		System.out.println(nextLine);
+		if(nextLine != null){
+			String [] ss = nextLine.split(",");
+			for(int i= 1; i< 94; i++){
+				//System.out.println(i + ": " + ss[i]);
+				instance.setValue((Attribute) main.wekaAttributes.elementAt(i), Integer.parseInt(ss[i]));
+
+			}
+			instance.setValue((Attribute) main.wekaAttributes.elementAt(0), ss[94]);
+
+		}
+		else{
+			System.out.println("End of File reached.");
+		}
+		return instance;
+	}
+
+}
